a779fb5b821a18cc50a4565bf6b462ee4030a8a9
xinvxueyuan <xinvxueyuan@yeah.net>
2025-12-06 21:21:36 +0800

    refactor(webui): 重构webui挂载逻辑并更新配置处理
    重构webui挂载逻辑，将mount方法重命名为index以更准确反映其功能。添加对webui配置的检查，仅在启用时运行。同时更新.env.example中的端口注释以更清晰描述用途。

--- src/builtin_plugins/lingchu_bot/core/web/mount.py
+++ src/builtin_plugins/lingchu_bot/core/web/mount.py
@@ -3,22 +3,31 @@ from nicegui import ui
 from nonebot import require
 
 require("lingchu_bot")
+from nonebot import get_plugin_config
 
+from ...config import Config
 
-ui.run_with(
-    app=nonebot.get_asgi(),
-    mount_path="/webui",
-    title="灵初web后台",
-    dark=None,
-    language="zh-CN",
-)
+webui_config = get_plugin_config(Config)
+global_config = nonebot.get_driver().config
+
+if webui_config.webui_enabled:
+    web_env = global_config.environment == "prod"
+    ui.run_with(
+        app=nonebot.get_asgi(),
+        mount_path="/webui",
+        title="灵初web后台",
+        dark=None,
+        storage_secret=webui_config.webui_token,
+        prod_js=web_env,
+        language="zh-CN",
+    )
 
 
 class BaseMount:
     def __init__(self) -> None:
-        self.mount()
+        self.index()
 
-    def mount(self) -> None:
+    def index(self) -> None:
         @ui.page("/")
         def _() -> None:
             with ui.header():
